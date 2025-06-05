9205fb2189b9e348075d0548b7566385afbaa99e
Jaylonnet <jaylonwingfield10@gmail.com>
2025-06-05 21:32:04 +0100

    feat: create card

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
+import image from "./assets/avatar-jessica.jpeg";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="container">
+        <div className="card">
+          <div className="avatar">
+            <img src={image} alt="" />
+          </div>
+          <div className="full-name">Jessica Randall</div>
+          <div className="location">London, United Kingdom</div>
+          <div className="sub-text">"Front-end developer and avid reader."</div>
+          <ul className="link-list">
+            <li>
+              <a href="#" className="link">
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link">
+                Frontend Mentor
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link">
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link">
+                Twitter
+              </a>
+            </li>
+            <li>
+              <a href="#" className="link">
+                Instagram
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
